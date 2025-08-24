e7c693ca630d2746f2b83b6215b0234fd8056adf
Divyansh sharma <divyanshsharmakotarajasthan@gmail.com>
2025-08-25 00:59:26 +0530

    Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
+import Hero from "@/sections/Hero";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4 text-gray-700">
-        Hi, I'm Divyansh 👋
-      </h1>
-      <p className="text-lg text-gray-700 mb-6">
-        Full Stack Developer | Building things for the web
-      </p>
-      <Link
-        href="/projects"
-        className="px-6 py-3 bg-blue-600 text-white rounded-lg
-        shadow hover:bg-blue-700"
-      >
-        View Projects
-      </Link>
-    </section>
-  );
+  return <Hero />;
 }
---
+++ sections/Hero.tsx
@@ -0,0 +1,63 @@
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3, // delay between children
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0 },
+};
+
+export default function Hero() {
+  return (
+    <section className="min-h-screen flex flex-col md:flex-row items-center justify-between text-center px-6 md:px-16 lg:px-24">
+      {/* Left Content */}
+
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="flex-1 justify-center text-center space-y-6"
+      >
+        <motion.h1
+          variants={container}
+          className="text-4xl md:text-8xl font-bold"
+        >
+          Hi, I’m <span className="text-blue-600">Divyansh</span>
+        </motion.h1>
+        <motion.h2
+          variants={container}
+          className="text-xl md:text-5xl text-gray-600"
+        >
+          Full Stack Developer
+        </motion.h2>
+
+        {/* CTA Buttons */}
+        <motion.div
+          variants={container}
+          className="flex flex-col md:flex-row gap-4 justify-center "
+        >
+          <a
+            href="#projects"
+            className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
+          >
+            View My Work
+          </a>
+          <a
+            href="#contact"
+            className="px-6 py-3 border border-gray-400 rounded-lg hover:bg-gray-100 transition"
+          >
+            Contact Me
+          </a>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+}
